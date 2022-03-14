7564defe33e72e211a7bbdbb66f285ca8aca9a91
Paulo Padrao <55985904+pvpadrao@users.noreply.github.com>
2022-03-14 12:18:54 -0400

    .

---
+++ my_examples_pkg/scripts/simple_topic_subscriber.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+
+import rospy                                          
+from std_msgs.msg import Int32 
+
+def callback(msg):                                    # Define a function called 'callback' that receives a parameter 
+                                                      # named 'msg'
+    print (msg.data)                                  # Print the value 'data' inside the 'msg' parameter
+
+rospy.init_node('topic_subscriber')                   # Initiate a Node called 'topic_subscriber'
+
+sub = rospy.Subscriber('/counter', Int32, callback)   # Create a Subscriber object that will listen to the /counter
+                                                      # topic and will cal the 'callback' function each time it reads
+                                                      # something from the topic
+rospy.spin()
